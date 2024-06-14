d789f7e36a4321b66a2d7fbcc6fecb785948a0c3
victor-von-pooh <m0b9d3t0f-novacane-iamrenegade@ezweb.ne.jp>
2024-06-14 17:50:15 +0900

    ✨add Tensor transformation

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,9 +1,26 @@
+from typing import Literal
+
 import pandas as pd
+import torch
+from torch.utils.data import TensorDataset
 
 
 def df_index_resetter(df: pd.DataFrame) -> pd.DataFrame:
     df = df.reset_index().drop("index", axis=1)
     return df
+
+
+def to_torch(
+    df: pd.DataFrame, torch_type: Literal["Float", "Long"]="Float"
+) -> torch.Tensor:
+    data = df.to_numpy()
+
+    if torch_type=="Float":
+        data = torch.from_numpy(data).float()
+    else:
+        data = torch.from_numpy(data).long()
+    
+    return data
 
 
 def make_datasets(cfg: dict) -> dict:
@@ -22,10 +39,21 @@ def make_datasets(cfg: dict) -> dict:
     train_df = df_index_resetter(train_df)
     valid_df = df_index_resetter(valid_df)
 
+    org_torch = to_torch(df, cfg["torch_type"])
+    train_torch = to_torch(train_df, cfg["torch_type"])
+    valid_torch = to_torch(valid_df, cfg["torch_type"])
+
+    org_dataset = TensorDataset(org_torch)
+    train_dataset = TensorDataset(train_torch)
+    valid_dataset = TensorDataset(valid_torch)
+
     out = {
         "org_data": df,
+        "org_dataset": org_dataset,
         "train_data": train_df,
-        "valid_data": valid_df
+        "train_dataset": train_dataset,
+        "valid_data": valid_df,
+        "valid_dataset": valid_dataset
     }
 
     return out
