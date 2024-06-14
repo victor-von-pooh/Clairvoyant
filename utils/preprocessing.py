fdecfc17c1819d780633a2bf97ea5b8ba6b14f27
victor-von-pooh <m0b9d3t0f-novacane-iamrenegade@ezweb.ne.jp>
2024-06-14 17:19:01 +0900

    🎉create preprocessing

---
+++ utils/preprocessing.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+
+def get_dataframe(cfg: dict) -> pd.DataFrame:
+    df = pd.read_csv(cfg["data_path"])
+    return df
+
+
+def df_index_resetter(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.reset_index().drop("index", axis=1)
+    return df
+
+
+def make_datasets(df: pd.DataFrame, cfg: dict) -> dict:
+    pre_df = df.copy()
+
+    sampling_rate = cfg["sampling_rate"]["train"] + cfg["sampling_rate"]["valid"]
+    train_rate = cfg["sampling_rate"]["train"] / sampling_rate
+    sampled_df = pre_df.sample(frac=sampling_rate, random_state=cfg["seed"])
+
+    train_df = sampled_df.sample(frac=train_rate, random_state=cfg["seed"])
+    train_index = train_df.index
+
+    valid_df = sampled_df.drop(train_index)
+
+    train_df = df_index_resetter(train_df)
+    valid_df = df_index_resetter(valid_df)
+
+    out = {
+        "org_data": df,
+        "train_data": train_df,
+        "valid_data": valid_df
+    }
+
+    return out
