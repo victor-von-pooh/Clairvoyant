16e8a3736491128259bce2e0ca588b10603a6756
victor-von-pooh <m0b9d3t0f-novacane-iamrenegade@ezweb.ne.jp>
2024-06-16 20:57:07 +0900

    ♻️update typing details

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -35,7 +35,10 @@ def to_torch(
     return data, ss
 
 
-def make_datasets(cfg: dict) -> dict:
+def make_datasets(cfg: dict) -> dict[
+    pd.DataFrame, pd.DataFrame, TensorDataset,
+    pd.DataFrame, TensorDataset, StandardScaler
+]:
     df = pd.read_csv(cfg["data_path"])
     pre_df = df.copy()
 
